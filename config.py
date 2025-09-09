0bd4c2bd86102079a0eea3c04f29fdeb92d87f3c
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2025-09-08 22:11:33 -0600

    Agregar configuración para la ubicación y tipo de token JWT en los headers

--- config.py
+++ config.py
@@ -13,6 +13,10 @@ class Config:
     JWT_SECRET_KEY = os.environ.get(
         'JWT_SECRET_KEY') or 'jwt-secret-string-change-in-production'
     JWT_ACCESS_TOKEN_EXPIRES = 3600  # 1 hora
+    # Asegurar que busca el token en los headers
+    JWT_TOKEN_LOCATION = ['headers']
+    JWT_HEADER_NAME = 'Authorization'
+    JWT_HEADER_TYPE = 'Bearer'
 
 
 class DevelopmentConfig(Config):
