733ac004c377e2fdae5cc758afb40bb55c60f2c4
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2025-10-31 14:59:51 -0600

    Corrige la referencia de revisión en la migración para agregar columnas public_slug a eventos y actividades

--- migrations/versions/20251009_add_public_slug_columns.py
+++ migrations/versions/20251009_add_public_slug_columns.py
@@ -1,7 +1,7 @@
 """add public_slug columns to events and activities
 
 Revision ID: 20251009_add_public_slug_columns
-Revises:
+Revises: 20250925_add_activity_fields
 Create Date: 2025-10-09 00:00:00.000000
 """
 
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = "20251009_add_public_slug_columns"
-down_revision = None
+down_revision = "20250925_add_activity_fields"
 branch_labels = None
 depends_on = None
 
