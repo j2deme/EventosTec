e2520512198cf3f5d649b23eec11a8409a6cb286
Jaime Jesús Delgado Meraz <j2deme@gmail.com>
2025-12-05 10:15:16 -0600

    Agrega script de despliegue y comenta la línea de mapeo de volúmenes para desarrollo local

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# deploy.sh
+
+echo "🕗 Actualizando código..."
+git pull origin main
+
+echo "🏗️  Reconstruyendo imagen..."
+docker-compose build --pull --no-cache web
+
+echo "🔄 Reiniciando servicio..."
+docker-compose up -d --force-recreate --no-deps web
+
+echo "✅ Despliegue completado."
